9b9af60db673bb7c1aaab3585faa1be8ab0fdbe1
Carmine-Doronzo <carmine.doronzo92@gmail.com>
2024-06-03 17:52:56 +0200

    Create a admin index view

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\ProjectController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -22,9 +23,11 @@ Route::middleware(['auth', 'verified'])
 ->name('admin.') 
 ->prefix('admin') 
 ->group(function(){
-    Route::get('/admin', function () {
+    Route::get('/', function () {
         return view('admin.dashboard');
     })->name('dashboard');
+
+    Route::resource('projects', ProjectController::class);
 });
 
 Route::middleware('auth')->group(function () {
